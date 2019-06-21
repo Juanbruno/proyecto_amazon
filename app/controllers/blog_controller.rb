0e4a7b4bc9cc3ccf9a397ecf1d69e22f6db17e5f
Juan bruno <jpruizk9@gmail.com>
2019-06-20 19:39:01 -0500

    cambio1

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,28 @@
+class BlogController < ApplicationController
+
+	def index
+	end
+
+	def about
+	end
+
+	def contact
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
+
+end
